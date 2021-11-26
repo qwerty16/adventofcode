8e15dd75c8b5ea4611ff5e3e1bfb74352800f8f6
Rachel Armstrong <rarmstrong573@gmail.com>
2021-11-26 07:25:35 +0000

    day 1 challenge 2

--- 2020/day 1/main.py
+++ 2020/day 1/main.py
@@ -1,6 +1,6 @@
 values = []
 
-with open("input.txt", 'r') as f:
+with open(r"C:\Users\rarms\Documents\Programming\Python\adventofcode\adventofcode\2020\day 1\input.txt", 'r') as f:
     for line in f.readlines():
         values += [int(line)]
 
@@ -8,12 +8,16 @@ target = 2020
 
 copy1 = values[:]
 copy2 = values[:]
+copy3 = values[:]
+
+counter = 0
 
 for i in copy1:
-    for j in copy2:
-        result = i + j
-        if result == target:
-            print(f"{i} + {j} = {result}")
-            output = i * j
-            print(f"{i} * {j} = {output}")
-    copy2 = copy2[1:]
+    for j in copy2[counter+1:]:
+        for k in copy3[counter+2:]:
+            result = i + j + k
+            if result == target:
+                print(f"{i} + {j} + {k} = {result}")
+                output = i * j * k
+                print(f"{i} * {j} * {k}= {output}")
+
