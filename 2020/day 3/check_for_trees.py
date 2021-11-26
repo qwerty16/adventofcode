21501fd7258fedfbbeaa35ef26d6abf545dd0246
Rachel Armstrong <rarmstrong573@gmail.com>
2021-11-26 19:31:22 +0000

    day 3 part 2

--- 2020/day 3/check_for_trees.py
+++ 2020/day 3/check_for_trees.py
@@ -3,17 +3,28 @@ import os
 path = os.getcwd()
 input_file_path = os.path.join(path, "input.txt")
 
-position = 0
-total = 0
+directions = ((1,1), (3,1), (5,1), (7,1), (1,2))
+total = []
 
-with open(input_file_path) as input_file:
-    for line in input_file.readlines():
-        line = line.strip()
-        target = line[position]
-        print(f"Character: {target}")
-        if target == "#":
-            total += 1
-        position += 3
-        position = position % len(line)
+def trees_in_path(input_path, horizontal_step, vertical_step=1):
+    position = 0
+    total = 0
+    line_number = 0
+    with open(input_path) as input_file:
+        for line in input_file.readlines():
+            if line_number % vertical_step == 0:
+                line = line.strip()
+                target = line[position]
+                if target == "#":
+                    total += 1
+                position += horizontal_step
+                position = position % len(line)
+            line_number += 1
+    return total
+
+for direction in directions:
+    total += [trees_in_path(input_file_path, direction[0], direction[1])]
 
 print(f"Total number of trees: {total}")
+answer = total[0] * total[1] * total[2] * total[3] * total[4]
+print(f"Answer = {total[0]} * {total[1]} * {total[2]} * {total[3]} * {total[4]} = {answer}")
