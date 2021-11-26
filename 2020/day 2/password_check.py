51fe2a9d1068208314f2bad1e700d98f2b0182db
Rachel Armstrong <rarmstrong573@gmail.com>
2021-11-26 08:25:10 +0000

    day 2 challenge 1

--- 2020/day 2/password_check.py
+++ 2020/day 2/password_check.py
@@ -2,3 +2,29 @@ import os
 
 path = os.getcwd()
 input_file_path = os.path.join(path, "input.txt")
+
+result = 0
+
+correct_passwords = []
+
+def password_meets_rule(rule, password):
+    [a, character] = rule.split()
+    [min, max] = a.split("-")
+    min = int(min)
+    max = int(max)
+    total = password.count(character)
+    if total >= min and total <= max:
+        return True
+    else:
+        return False
+
+with open(input_file_path, 'r') as input_file:
+    for line in input_file.readlines():
+        line = line.strip()
+        [rule, password] = line.split(":")
+        if password_meets_rule(rule, password):
+            result += 1
+            correct_passwords += [line]
+
+print(f"Number of valid passwords: {result}")
+print(correct_passwords)
