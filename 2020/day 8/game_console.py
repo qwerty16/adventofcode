64f0449fbc779e9ddeec01119b8a83b29a8c6636
Rachel Armstrong <rarmstrong573@gmail.com>
2021-11-30 22:36:31 +0000

    2020 day 8 input

---
+++ 2020/day 8/game_console.py
@@ -0,0 +1,6 @@
+from os import linesep
+from pathlib import Path
+
+test_input_file_path = Path(__file__).absolute().parent / "test_input.txt"
+input_file_path = Path(__file__).absolute().parent / "input.txt"
+
