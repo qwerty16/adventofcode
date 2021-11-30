7ad126603ded068c1ffe94f821099d29011735a3
Rachel Armstrong <rarmstrong573@gmail.com>
2021-11-30 22:43:27 +0000

    2020 day 8 part 1 input read into list of lists

--- 2020/day 8/game_console.py
+++ 2020/day 8/game_console.py
@@ -4,3 +4,24 @@ from pathlib import Path
 test_input_file_path = Path(__file__).absolute().parent / "test_input.txt"
 input_file_path = Path(__file__).absolute().parent / "input.txt"
 
+def file_to_list(input_file_path):
+    output = []
+    with open(input_file_path, 'r') as input_file:
+        for line in input_file.readlines():
+            output += [[line.strip(), 0]]
+    
+    return output
+
+print(file_to_list(test_input_file_path))
+
+
+# >>> e = [['a', 0], ['b', 0], ['c', 0]]
+# >>> e[0]
+# ['a', 0]
+# >>> e[0][0]
+# 'a'
+# >>> e[0][1] 
+# 0
+# >>> e[0][1] = 1
+# >>> e
+# [['a', 1], ['b', 0], ['c', 0]]
