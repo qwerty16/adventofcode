4e5ad13d7136d0b0e206354015790fdbbb94b524
Rachel Armstrong <rarmstrong573@gmail.com>
2021-12-02 06:39:46 +0000

    2021 day 2 part 2

--- 2021/day 2/location.py
+++ 2021/day 2/location.py
@@ -5,43 +5,59 @@ from typing import ParamSpecArgs
 test_input_file_path = Path(__file__).absolute().parent / "test_input.txt"
 input_file_path = Path(__file__).absolute().parent / "input.txt"
 
-def distance_change(line):
+def process_line(line):
     line = line.strip()
     direction, magnitude = line.split()
     magnitude = int(magnitude)
+    return direction, magnitude
 
-    horizontal = 0
-    depth = 0
+def aim(direction, magnitude, current):
+    horizontal, depth, aim = current
 
     if direction == 'forward':
-        horizontal = magnitude
+        horizontal += magnitude
+        depth += aim * magnitude
     elif direction == 'down':
-        depth = magnitude
+        aim += magnitude
     elif direction == 'up':
-        depth = -magnitude
-    
-    return (horizontal, depth)
+        aim -= magnitude
 
-def calculate_distance_traveled(input_file_path):
+    return (horizontal, depth, aim)
+
+
+def cardinal(direction, magnitude, current):
+    horizontal, depth, aim = current
+
+    if direction == 'forward':
+        horizontal += magnitude
+    elif direction == 'down':
+        depth += magnitude
+    elif direction == 'up':
+        depth -= magnitude
+    
+    return (horizontal, depth, aim)
+
+def calculate_distance_traveled(input_file_path, movement):
     horizontal = 0
     depth = 0
+    aim = 0
 
     with open(input_file_path) as input_file:
         for line in input_file.readlines():
-            h_change, d_change = distance_change(line)
-            horizontal += h_change
-            depth += d_change
+            direction, magnitude = process_line(line)
+            current = horizontal, depth, aim
+            horizontal, depth, aim = movement(direction, magnitude, current)
 
     return horizontal * depth
 
 print("### Part 1")
-test_output = calculate_distance_traveled(test_input_file_path)
-actual_output = calculate_distance_traveled(input_file_path)
+test_output = calculate_distance_traveled(test_input_file_path, cardinal)
+actual_output = calculate_distance_traveled(input_file_path, cardinal)
 print(f"Test: {test_output}\t\tShould be: 150")
 print(f"Actual: {actual_output}")
 
 print("### Part 2")
-test_output = calculate_distance_traveled(test_input_file_path)
-#actual_output = calculate_distance_traveled(input_file_path)
-print(f"Test: {test_output}\t\tShould be: XXX")
-#print(f"Actual: {actual_output}")
+test_output = calculate_distance_traveled(test_input_file_path, aim)
+actual_output = calculate_distance_traveled(input_file_path, aim)
+print(f"Test: {test_output}\t\tShould be: 900")
+print(f"Actual: {actual_output}")
