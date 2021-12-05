6d08466bed249788384a6c2c569cf5ee9a0dd40c
Rachel Armstrong <rarmstrong573@gmail.com>
2021-12-05 20:02:05 +0000

    updated template

--- 2021/template_day_X/main.py
+++ 2021/template_day_X/main.py
@@ -1,20 +1,21 @@
-from os import linesep
 from pathlib import Path
 
 test_input_file_path = Path(__file__).absolute().parent / "test_input.txt"
 input_file_path = Path(__file__).absolute().parent / "input.txt"
 
+
 def main(input_file_path):
     return 0
 
+
 print("### Part 1")
 test_output = main(test_input_file_path)
-#actual_output = main(input_file_path)
+# actual_output = main(input_file_path)
 print(f"Test: {test_output}\t\tShould be: XXX")
-#print(f"Actual: {actual_output}")
+# print(f"Actual: {actual_output}")
 
 print("### Part 2")
-test_output = main(test_input_file_path)
-#actual_output = main(input_file_path)
-print(f"Test: {test_output}\t\tShould be: XXX")
-#print(f"Actual: {actual_output}")
+# test_output = main(test_input_file_path)
+# actual_output = main(input_file_path)
+# print(f"Test: {test_output}\t\tShould be: XXX")
+# print(f"Actual: {actual_output}")
