fc27f1dfbc7da1e3f4d29c91b90177dda2ac313c
Rachel Armstrong <rarmstrong573@gmail.com>
2021-12-07 07:05:34 +0000

    day 7 part 1

--- 2021/day 7/crabs.py
+++ 2021/day 7/crabs.py
@@ -4,15 +4,34 @@ test_input_file_path = Path(__file__).absolute().parent / "test_input.txt"
 input_file_path = Path(__file__).absolute().parent / "input.txt"
 
 
+def fuel_cost(lst, position):
+    distances = []
+    for i in lst:
+        distances += [abs(i - position)]
+    return sum(distances)
+
+
 def main(input_file_path):
-    return 0
+    with open(input_file_path) as input_file:
+        inp = input_file.readline().strip().split(",")
+    crabs = []
+    for i in inp:
+        crabs += [int(i)]
+
+    previous_cost = 1000000000
+    for position in range(max(crabs)):
+        cost = fuel_cost(crabs, position)
+        if cost < previous_cost:
+            previous_cost = cost
+        else:
+            return position - 1, previous_cost
 
 
 print("### Part 1")
 test_output = main(test_input_file_path)
-# actual_output = main(input_file_path)
-print(f"Test: {test_output}\t\tShould be: 71")
-# print(f"Actual: {actual_output}")
+actual_output = main(input_file_path)
+print(f"Test: {test_output}\t\tShould be: Position 2 Fuel 37")
+print(f"Actual: {actual_output}")
 
 print("### Part 2")
 # test_output = main(test_input_file_path)
