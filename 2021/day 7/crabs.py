a46b916fd9b75212a99b1cfad544fe5fd0363fc1
Rachel Armstrong <rarmstrong573@gmail.com>
2021-12-07 07:18:06 +0000

    day 7 part 2

--- 2021/day 7/crabs.py
+++ 2021/day 7/crabs.py
@@ -4,14 +4,22 @@ test_input_file_path = Path(__file__).absolute().parent / "test_input.txt"
 input_file_path = Path(__file__).absolute().parent / "input.txt"
 
 
-def fuel_cost(lst, position):
+def linear_fuel_cost(lst, position):
     distances = []
     for i in lst:
         distances += [abs(i - position)]
     return sum(distances)
 
 
-def main(input_file_path):
+def triangular_fuel_cost(lst, position):
+    distances = []
+    for i in lst:
+        n = abs(i - position)
+        distances += [(n ** 2 + n) / 2]
+    return sum(distances)
+
+
+def align_crabs(input_file_path, movement="linear"):
     with open(input_file_path) as input_file:
         inp = input_file.readline().strip().split(",")
     crabs = []
@@ -20,7 +28,10 @@ def main(input_file_path):
 
     previous_cost = 1000000000
     for position in range(max(crabs)):
-        cost = fuel_cost(crabs, position)
+        if movement == "linear":
+            cost = linear_fuel_cost(crabs, position)
+        elif movement == "triangular":
+            cost = triangular_fuel_cost(crabs, position)
         if cost < previous_cost:
             previous_cost = cost
         else:
@@ -28,13 +39,13 @@ def main(input_file_path):
 
 
 print("### Part 1")
-test_output = main(test_input_file_path)
-actual_output = main(input_file_path)
+test_output = align_crabs(test_input_file_path)
+actual_output = align_crabs(input_file_path)
 print(f"Test: {test_output}\t\tShould be: Position 2 Fuel 37")
 print(f"Actual: {actual_output}")
 
 print("### Part 2")
-# test_output = main(test_input_file_path)
-# actual_output = main(input_file_path)
-# print(f"Test: {test_output}\t\tShould be: XXX")
-# print(f"Actual: {actual_output}")
+test_output = align_crabs(test_input_file_path, movement="triangular")
+print(f"Test: {test_output}\t\tShould be: Position 5 Fuel 168")
+actual_output = align_crabs(input_file_path, movement="triangular")
+print(f"Actual: {actual_output}")
