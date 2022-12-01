04c3d458a08466cac09a227a201d4bf904ec9ef9
Rachel Armstrong <rarmstrong573@gmail.com>
2022-12-01 17:41:26 +0000

    2021 day 9

--- 2021/day 9/main.py
+++ 2021/day 9/main.py
@@ -22,30 +22,58 @@ def less_than(value, list1):
     return True
 
 
-def low_points(input_file_path):
-    heat_map = read_file(input_file_path)
-    output = 0
+def adj_points(x, y, heat_map):
+    adj = []
+    co_ords = ((x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1))
+    for point in co_ords:
+        if point[0] >= 0 and point[1] >= 0:
+            try:
+                adj += [heat_map[point[0]][point[1]]]
+            except IndexError:
+                pass
+    return adj
+
+
+def low_points(heat_map):
+    output = []
     for x, column in enumerate(heat_map):
         for y, value in enumerate(column):
-            adj = []
-            co_ords = ((x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1))
-            for point in co_ords:
-                if point[0] >= 0 and point[1] >= 0:
-                    try:
-                        adj += [heat_map[point[0]][point[1]]]
-                    except IndexError:
-                        pass
+            adj = adj_points(x, y, heat_map)
 
             if less_than(value, adj):
-                output += value + 1
+                output += [value]
 
     return output
 
 
+def basin_size(x, y, heat_map):
+    pass
+
+
+def part1(input_file_path):
+    heat_map = read_file(input_file_path)
+    points = low_points(heat_map)
+    risk = 0
+    for point in points:
+        risk += point + 1
+    return risk
+
+
+def part2(input_file_path):
+    heat_map = read_file(input_file_path)
+    points = low_points(heat_map)
+    basin_sizes = []
+    for point in points:
+        basin_sizes += [basin_size(point[0], point[1], heat_map)]
+
+    basin_sizes.sort(reverse=True)
+    return basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
+
+
 print("### Part 1")
-test_output = low_points(test_input_file_path)
+test_output = part1(test_input_file_path)
 print(f"Test: {test_output}\t\tShould be: 15")
-actual_output = low_points(input_file_path)
+actual_output = part1(input_file_path)
 print(f"Actual: {actual_output}")
 
 print("### Part 2")
