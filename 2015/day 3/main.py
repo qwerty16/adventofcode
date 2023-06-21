11d926fc23c3d08338695a303d6362134a757962
Rachel Armstrong <rarmstrong573@gmail.com>
2023-06-21 10:54:25 +0100

    2015 day 3 part 2

--- 2015/day 3/main.py
+++ 2015/day 3/main.py
@@ -7,46 +7,37 @@ input_file_path = Path(__file__).absolute().parent / "input.txt"
 Point = namedtuple("Point", ["x", "y"])
 
 
+def move(location, direction):
+    if direction == ">":  # right
+        return Point(location.x + 1, location.y)
+    elif direction == "<":  # left
+        return Point(location.x - 1, location.y)
+    elif direction == "^":  # up
+        return Point(location.x, location.y + 1)
+    elif direction == "v":  # down
+        return Point(location.x, location.y - 1)
+    else:
+        print(f"Error: unable to parse direction {direction}")
+
+
 def main(input_file_path, part=1):
     visited = set()
     santa_loc = Point(0, 0)
     robo_loc = Point(0, 0)
     santa_moves = False
     with open(input_file_path) as input_file:
-        while True:
-            visited.add(santa_loc)
-            visited.add(robo_loc)
-            santa_moves = not santa_moves
-            c = input_file.read(1)
-            if santa_moves:
-                if not c:
-                    break
-                elif c == ">":  # right
-                    santa_loc = Point(santa_loc.x + 1, santa_loc.y)
-                elif c == "<":  # left
-                    santa_loc = Point(santa_loc.x - 1, santa_loc.y)
-                elif c == "^":  # up
-                    santa_loc = Point(santa_loc.x, santa_loc.y + 1)
-                elif c == "v":  # down
-                    santa_loc = Point(santa_loc.x, santa_loc.y - 1)
-            else:
-                if not c:
-                    break
-                elif c == ">":  # right
-                    robo_loc = Point(santa_loc.x + 1, santa_loc.y)
-                elif c == "<":  # left
-                    robo_loc = Point(santa_loc.x - 1, santa_loc.y)
-                elif c == "^":  # up
-                    robo_loc = Point(santa_loc.x, santa_loc.y + 1)
-                elif c == "v":  # down
-                    robo_loc = Point(santa_loc.x, santa_loc.y - 1)
+        input = input_file.read()
+
+    for direction in input:
+        visited.add(santa_loc)
+        visited.add(robo_loc)
+        santa_moves = not santa_moves
+        if santa_moves:
+            santa_loc = move(santa_loc, direction)
+        else:
+            robo_loc = move(robo_loc, direction)
     return len(visited)
 
 
-print("### Part 1")
 actual_output = main(input_file_path)
 print(f"Actual: {actual_output}")
-
-print("### Part 2")
-# actual_output = main(input_file_path)
-# print(f"Actual: {actual_output}")
